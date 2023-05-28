2537b518a24fbaf53899beb2e51bc4edbf4ee75b
Tahir <rajatahirali236@gmail.com>
2023-05-28 12:45:23 +0500

    shirts fix

--- src/landingPage/Testamonial.jsx
+++ src/landingPage/Testamonial.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import shirt1 from '../assests/images/shirt1.png';
-import shirt2 from '../assests/images/shirt2.png';
-import shirt3 from '../assests/images/shirt3.png';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import shirt1 from "../assests/images/shirt1.png";
+import shirt2 from "../assests/images/shirt2.png";
+import shirt3 from "../assests/images/shirt3.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Testamonial() {
   useEffect(() => {
@@ -14,15 +14,52 @@ export default function Testamonial() {
   }, []);
 
   return (
-    <div className='d-flex justify-content-center' style={{ display: 'flex' }}>
-      <div className='d-flex justify-content-center' data-aos='fade-left' data-aos-delay='200'>
-        <img src={shirt1} alt='' style={{ cursor: 'pointer', width: '90%', minHeight:"400px" }} />
-      </div>
-      <div className='d-flex justify-content-center' data-aos='fade-right' data-aos-delay='400'>
-        <img src={shirt2} alt='' style={{ cursor: 'pointer', width: '90%' , minHeight:"400px" }} />
-      </div>
-      <div className='d-flex justify-content-center' data-aos='fade-left' data-aos-delay='600'>
-        <img src={shirt3} alt='' style={{ cursor: 'pointer', width: '90%', minHeight:"400px" }} />
+    <div className="container">
+      <div className="row">
+        <div
+          className="col-sm-12 col-lg-4 "
+          data-aos="fade-left"
+          data-aos-delay="200"
+        >
+          <img
+            src={shirt2}
+            alt=""
+            style={{ cursor: "pointer", width: "500px", height: "500px" }}
+          />
+        </div>
+        <div
+          className="col-sm-12 col-lg-4 "
+          data-aos="fade-right"
+          data-aos-delay="400"
+        >
+          <img
+            src={shirt3}
+            alt=""
+            style={{
+              cursor: "pointer",
+              width: "550px",
+              height: "550px",
+              marginTop: "-40px",
+              marginLeft: "-40px",
+            }}
+          />
+        </div>
+        <div
+          className="col-sm-12 col-lg-4 "
+          data-aos="fade-left"
+          data-aos-delay="600"
+        >
+          <img
+            src={shirt1}
+            alt=" "
+            style={{
+              cursor: "pointer",
+              width: "500px",
+              height: "500px",
+              marginLeft: "-40px",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
